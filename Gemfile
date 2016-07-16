9e2ac53831a93fa2fa13a289eb66692c9a23294e
alienfungi <pzwoodfin@gmail.com>
2016-07-16 17:35:14 -0400

    Configures site for puma, nginx, and upstart.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.3.0'
 
 
+gem 'foreman'
 # Web server
 gem 'puma'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,25 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+threads_max = Integer(ENV['MAX_THREADS'] || 6)
+threads_min = Integer(ENV['MIN_THREADS'] || 1)
+threads threads_min, threads_max
 
 preload_app!
 
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{ app_dir }/shared"
+rails_env = ENV['RACK_ENV'] || 'development'
+
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment rails_env
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+# Socket location
+bind "unix://#{ shared_dir }/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{ shared_dir }/log/puma.stdout.log", "#{ shared_dir }/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{ shared_dir }/pids/puma.pid"
+state_path "#{ shared_dir }/pids/puma.state"
+activate_control_app
